6233e81f168a01377b8425b08dcc74b0568702ae
ciara-picou <ciarapicou@att.net>
2021-03-22 15:51:43 -0500

    added extractMax()

--- script.js
+++ script.js
@@ -1,35 +1,119 @@
 class MaxBinaryHeap {
-    constructor(){
-        this.values = [];
+  constructor() {
+    this.values = [];
+  }
+  insert(element) {
+    //push the value into the values property on the heap
+    this.values.push(element);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    // We will begin by adding new elements to the end of the array
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    //console.log("element", element);
+    while (idx > 0) {
+      //remember to find a parent index we have the formula Math.floor(n -1)/2
+      //we floor it because indices must be whole numbers
+      let parentIdx = Math.floor((idx - 1) / 2);
+      //we access the parent element by its index
+      let parent = this.values[parentIdx];
+      //the loop will run until the element has found its place
+      //beneath a parent with a greater value
+      // REMEMBER, in a MaxBinary heap, the parent must be greater
+      //than each of its children
+      if (element <= parent) break;
+      //thisloop will continue until either the element in question has a smaller value
+      // than its parent or an equal value to its parent
+      // we switch their positions in the values array
+      //the larger element bubbles up one level and the loop continues
+      // only next time we are comparing the parent of this loop with its parent
+      console.log("before", "this.values[parentIdx]", this.values[parentIdx]);
+      console.log("before", "this.values[idx]", this.values[idx]);
+      console.log("before", "idx", idx);
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+      console.log("after", "this.values[parentIdx]", this.values[parentIdx]);
+      console.log("after", "this.values[idx]", this.values[idx]);
+      console.log("after", "idx", idx);
+      console.log("values array", this.values);
     }
-    insert(element){
-        //push the value into the values property on the heap
-        this.values.push(element);
-        this.bubbleUp();
-    }
-    bubbleUp(){
+  }
+  extractMax() {
+    // set a variable max to equal the first element from the values array
+    //remember, in a max heap, the root will always contain the highest value
+    const max = this.values[0];
+    //remove the last element from the values and contain it in a variable
+    const end = this.values.pop();
 
-        let idx = this.values.length - 1;
-        const element = this.values[idx];
-        while(idx > 0){
-            let parentIdx = Math.floor((idx - 1)/2);
-            let parent = this.values[parentIdx];
-            //the loop will run until the element has found its place 
-            //beneath a parent with a greater value
-            // REMEMBER, in a MaxBinary heap, the parent must be greater 
-            //than each of its children 
-            if(element <= parent) break;
-             //thisloop will continue until either the element in question has a smaller value 
-             // than its parent or an equal value to its parent
-            // we switch their positions in the values array
-            //the larger element bubbles up one level and the loop continues
-            // only next time we are comparing the parent of this loop with its parent
-            this.values[parentIdx] = element;
-            this.values[idx] = parent;
-            idx = parentIdx;
-           
-        }
+    // we need this condition to take care of the edge case when there is only one element
+    //in  the this.values array. Without this conditional
+    //we would simply be removing the only element with end= this.values.pop()
+    //and then adding it back with this.values[0] =  end
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      //Now we must bubbleDown
+      this.bubbleDown();
     }
+
+    console.log(max);
+    return max;
+  }
+  bubbleDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIndex = 2 * idx + 1;
+      let rightChildIndex = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+      //this conditional makes sure that the index for left child is
+      // not greater than the amount of elements we have in our values array
+      //aka it checks whether leftChildIndex is out of bounds
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild > element) {
+          //this variable swap is going to keep track
+          //of the position that we are going to swap with
+          //so far a swap will only be made if the left child is larger than the element
+          swap = leftChildIndex;
+        }
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+      }
+      // now there are two scenarios where we would want to swap with right
+      //1. the left child was smaller than the element (and no swap has occured)
+      //but the right child was larger than the element
+      //2. the left child was larger than the element(aka a swap has occured)
+      //but the right child  was larger than the left child
+      if (
+        (swap === null && rightChild > element) ||
+        (swap !== null && rightChild > leftChild)
+      ) {
+        swap = rightChildIndex;
+      }
+      //if no swap has occured by this point then
+      //the parent is greater than its children and the order is correct
+      // our work is done here we may break out of our loop
+      if (swap === null) break;
+      //if not then we must actually make the swap and continue to the next iteration
+
+      console.log("before swap", this.values[idx], "this.values[idx]");
+      console.log("before swap", element, "element");
+      console.log("before swap", this.values, "this.values");
+
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+      console.log("after swap", this.values[idx], "this.values[idx]");
+      console.log("after swap", element, "element");
+      console.log("after swap", this.values, "this.values");
+    }
+  }
 }
 
 let heap = new MaxBinaryHeap();
@@ -40,3 +124,7 @@ heap.insert(18);
 heap.insert(27);
 heap.insert(12);
 heap.insert(55);
+
+heap.extractMax();
+heap.extractMax();
+console.log(heap.values);
